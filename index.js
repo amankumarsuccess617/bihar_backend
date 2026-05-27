1c0627f3e50d804e44b04d1e7091ec2022346990
amansuccess686 <aman@affiefly.co.in>
2026-05-28 00:45:03 +0530

    mast

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://bihar-frontend-kpx2-jnxbnnd3e.vercel.app",
   credentials: true
 }));
 app.use(express.json());
